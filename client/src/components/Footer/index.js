43b879fb0c300d5e3b16653275158466c96523c4
alciancio <alcianciotto@gmail.com>
2019-09-18 21:11:59 -0700

    Updates

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p className="pull-right">
+        <i className="fas fa-book" /> © Google Books Search</p>
+    </footer>
+  );
+}
+
+export default Footer;
